725c9ca95f0e098f366c74c285880b54d5559794
EddyChan1129 <22026266d@connect.polyu.hk>
2025-12-05 21:33:09 -0500

    added /test end point for demostrating Nignx's flow

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,14 @@ app.get("/api/hello", (req, res) => {
   });
 });
 
+app.get("/test", (req, res) => {
+  console.log("Host:", req.headers.host);
+  console.log("X-Real-IP:", req.headers["x-real-ip"]);
+  console.log("X-Forwarded-For:", req.headers["x-forwarded-for"]);
+  res.json({ message: "ok" });
+});
+
+
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Backend API listening on http://0.0.0.0:${PORT}`);
 });
